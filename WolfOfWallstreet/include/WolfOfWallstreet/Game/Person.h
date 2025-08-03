693b0e7180b4706c72e01026b53c492705efd9bf
Darthsae <sockmit2007@gmail.com>
2025-08-03 11:47:39 -0400

    Hopefully a fix?

--- WolfOfWallstreet/include/WolfOfWallstreet/Game/Person.h
+++ WolfOfWallstreet/include/WolfOfWallstreet/Game/Person.h
@@ -1,8 +1,13 @@
 #pragma once
 #ifndef PERSON_H
 #define PERSON_H
+namespace WolfOfWallstreet::Game {
+    struct Person;
+}
+
 #include <stdint.h>
 #include <vector>
+#include <WolfOfWallstreet/Game/Social/Relationship.h>
 #include <WolfOfWallstreet/Game/Time/Date.h>
 #include <WolfOfWallstreet/Game/Language/Name.h>
 #include <WolfOfWallstreet/Game/Data/SocialSecurityNumber.h>
